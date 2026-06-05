07eb126f7becacf0dde917971aa77eb4f19cea53
Pawan Pilgulwar <pilgulwarpawan2004@gmail.com>
2026-06-05 08:36:53 +0530

    Some update.

--- pet_rescue_pro/config/settings.py
+++ pet_rescue_pro/config/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@$z3*wct8t00xwoy-zf%w*_21o&u#8#z8!)#s=x=r0!#9j(mp+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('ENVIRONMENT') == 'development'
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.31.75", "pet-adoption-and-rescue-management-wkpz.onrender.com", "pet-adoption-rescue-management.vercel.app"]
 # ALLOWED_HOSTS = ["*"]
